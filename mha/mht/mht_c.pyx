dfbb775db99619e91e96ee0d5833d63658cece3e
Muhammed Uluyol <uluyo002@umn.edu>
2013-06-13 13:43:11 -0500

    Use Haversine for gating as well as geostrophic distance

--- mha/mht/mht_c.pyx
+++ mha/mht/mht_c.pyx
@@ -34,16 +34,8 @@ def geodesic_dist(lon1, lat1, lon2, lat2):
 	return get_dist(lon1, lat1, lon2, lat2)
 
 cdef bint gate(old_node, new_node, double gate_dist):
-	cdef double old_lon, new_lon, old_lat_rad, new_lat_rad, tmp, rad_dist, dist, adj_gate_dist
-	old_lat_rad = radians(old_node.obj.lat)
-	new_lat_rad = radians(new_node.obj.lat)
-	old_lon = old_node.obj.lon
-	new_lon = new_node.obj.lon
-	tmp = sin(old_lat_rad) * sin(new_lat_rad) +   \
-		cos(old_lat_rad) * cos(new_lat_rad) * \
-		cos(radians(fabs(new_lon-old_lon)))
-	rad_dist = acos(get_max(get_min(tmp, 1.0), -1.0)) # Force the number to strictly be within the domain
-	dist = rad_dist * 6371.01 # km
+	cdef double dist, adj_gate_dist
+	dist = get_dist(old_node.obj.lon, old_node.obj.lat, new_node.obj.lon, new_node.obj.lat)
 	adj_gate_dist = gate_dist - fabs(old_node.obj.lat)
 	if dist > adj_gate_dist:
 		return False
