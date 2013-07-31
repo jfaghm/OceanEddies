7fcf4bc07bf99f5961ef44b1101dcb7b9399a8ad
Muhammed Uluyol <uluyo002@umn.edu>
2013-07-31 14:21:52 -0500

    Vectorize mean_geo_speed (thanks Matt)

--- eddyscan/mean_geo_speed.m
+++ eddyscan/mean_geo_speed.m
@@ -3,29 +3,20 @@ function mean_speed = mean_geo_speed(ssh, pixels, lat, lon)
 % lat and lon should yield the correct values for indices of ssh.
     g = 980.665; % cm/s
     omega = 7.2921e-5;
-    us = zeros(size(pixels));
-    vs = zeros(size(pixels));
     [x, y] = ind2sub(size(ssh), pixels);
     lats = lat(x);
     f = 2*omega*sin(lats);
     f(f == 0) = 2*omega*sin(0.25); % f is coriolis frequency
-    for i = 1:length(pixels)
-        % Compute vs and us by taking the average with the difference of
-        % the next and previous pixel. Take into account wrap around for
-        % lons and caps for lats
-        dSSH_y = ssh(x(i), mod(y(i),size(ssh, 2))+1) - ...
-            ssh(x(i), mod(y(i)-2,size(ssh, 2))+1);
-        dSSH_x = ssh(min(x(i)+1,size(ssh,1)), y(i)) - ...
-            ssh(max(x(i)-1, 1), y(i));
-        dy = distance(lat(x(i)), lon(mod(y(i), size(ssh, 1))+1), ...
-            lat(x(i)), lon(mod(y(i)-2, size(ssh, 1))+1)) * 6371.01 * ...
-            100000 * pi / 180;
-        dx = (min(x(i)+1, size(ssh, 1))-max(1,x(i)-1))*111.12*100000* ...
-            180/(length(lats)-1);
-        vs(i) = -g*(dSSH_y)/(2*f(i)*dy);
-		us(i) =  g*(dSSH_x)/(2*f(i)*dx);
-    end
-    speeds = sqrt(us.^2+vs.^2);
-    mean_speed = mean(speeds);
+    
+    dSSH_y = ssh(sub2ind(size(ssh), x, mod(y, size(ssh, 2))+1)) - ...
+        ssh(sub2ind(size(ssh), x, mod(y-2, size(ssh, 2))+1));
+    dSSH_x = ssh(sub2ind(size(ssh), min(x+1, zeros(size(x)) + size(ssh, 1)), y))...
+        - ssh(sub2ind(size(ssh), max(x-1, ones(size(x))), y));
+    dy = distance(lat(x), lon(mod(y, size(ssh, 1))+1), lat(x), lon(mod(y-2, size(ssh, 1))+1)) .* ...
+        6371.01 .* 1000000 .* pi ./ 180;
+    dx = (min(x+1, size(ssh, 1)) - max(1, x-1)) .* 111.12 .* 1000000 .* 180 ./ (length(lats) - 1);
+    vs = -g .* (dSSH_y) ./ (2.*f .* dy);
+    us = g .* (dSSH_x) ./ (2 .* f .* dx );
+    speeds = sqrt(us .^2 + vs .^2);
+    mean_speed = nanmean(speeds);
 end
-
