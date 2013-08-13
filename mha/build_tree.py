c6d68aeb920988e0dd1daa02d260df15a12fad20
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-13 16:52:32 -0500

    Add example script for loading data Add print statements to example scripts

--- mha/build_tree.py
+++ mha/build_tree.py
@@ -4,5 +4,7 @@ import mht
 
 eddies_path = '/path/to/eddyscan/out'
 
+print 'Tracking...'
 roots = mht.build_mht(mht.list_eddies(eddies_path, 'eddies'), do_lookahead=True)
+print 'Saving...'
 mht.write_tracks(roots, 'cyclonic_tracks.mat', mht.list_dates(eddies_path, 'eddies'))
