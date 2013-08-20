07ef1629bb2bfbf3f1d20a014e8b2485ad3ca0aa
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-20 16:19:29 -0500

    Add date reference gen function

---
+++ viewer/mk_dateref.m
@@ -0,0 +1,10 @@
+function [ dateref ] = mk_dateref( tracks, dates )
+%MK_DATEREF Creates datereference matrix for tracks. This stores track
+% indicies for all tracks that are in the specific timestep.
+    dateref = cell(size(dates));
+    for i = 1:length(tracks)
+        for j = tracks{i}(:,3)
+            dateref{j} = [dateref{j} i];
+        end
+    end
+end
