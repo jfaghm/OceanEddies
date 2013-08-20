22df77d30a01f3efcb4c2bbf33103c59c0d3c947
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-20 16:37:40 -0500

    Add wrapper to help with scanning many timesteps at once

---
+++ eddyscan/scan_ssh_subset.m
@@ -0,0 +1,18 @@
+function scan_ssh_subset( ssh, lat, lon, dates, cyc, scan_type, destdir )
+%SCAN_SSH_SUBSET Scan all of the ssh data passed in (will function
+%correctly if data passed in is a subset)
+    if ~strcmp(destdir(end), filesep())
+        destdir = [destdir filesep()];
+    end
+    mkdir(destdir);
+    parfor i = 1:length(dates)
+        fprintf('starting iteration %d\n', i);
+        eddies = scan_single(ssh(:,:,i), lat, lon, dates(i), cyc, scan_type);
+        save_data([destdir cyc '_' num2str(dates(i))], eddies);
+        fprintf('finished iteration %d\n', i);
+    end
+end
+
+function save_data(path, eddies)
+    save(path, 'eddies', '-v7');
+end
