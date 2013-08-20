b0b96f5ec3989a1f1ecc5466a2e64fb6c7b75431
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-20 16:44:54 -0500

    Check for existance of directory before attempting to create it

--- eddyscan/scan_ssh_subset.m
+++ eddyscan/scan_ssh_subset.m
@@ -4,7 +4,9 @@ function scan_ssh_subset( ssh, lat, lon, dates, cyc, scan_type, destdir )
     if ~strcmp(destdir(end), filesep())
         destdir = [destdir filesep()];
     end
-    mkdir(destdir);
+    if exist(destdir, 'dir') ~= 7
+        mkdir(destdir);
+    end
     parfor i = 1:length(dates)
         fprintf('starting iteration %d\n', i);
         eddies = scan_single(ssh(:,:,i), lat, lon, dates(i), cyc, scan_type);
