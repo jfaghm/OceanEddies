32dfaad1d0854262983ccdb5a2d5ec03550132ab
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-22 14:32:29 -0500

    Transpose old tracks matrix to match lnn output

--- mha/export_tracks_mat.m
+++ mha/export_tracks_mat.m
@@ -1,7 +1,7 @@
 function [ old_tracks ] = export_tracks_mat( tracks )
 %EXPORT_TRACKS_MAT Will convert from new track format to old matrix based
 % tracks: new, struct based tracks list
-    old_tracks = cell(size(tracks));
+    old_tracks = cell(size(tracks)');
     for i = 1:length(tracks)
         old_tracks{i} = [tracks(i).Eddies.Lat ...
             tracks(i).Eddies.Lon ...
