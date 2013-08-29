1cd150e35695bb96e267daaba83817cc4ee7bfbd
Muhammed Uluyol <uluyo002@umn.edu>
2013-08-29 13:04:37 -0500

    Add track toggling

--- viewer/plot_tracks.m
+++ viewer/plot_tracks.m
@@ -15,7 +15,8 @@ function plot_tracks( tracks_cell, tracks_names, daterefs_cell, ...
     reset = false;
     loop = false;
     done = false;
-    bodiestoggle = false;
+    toggle_tracks = false;
+    show_tracks = true;
     i0 = 0;
 
     hdls.prev = uicontrol(hdls.fig, 'Style', 'pushbutton', 'Units', 'normalized', ...
@@ -25,7 +26,7 @@ function plot_tracks( tracks_cell, tracks_names, daterefs_cell, ...
     hdls.done = uicontrol(hdls.fig, 'Style', 'pushbutton', 'Units', 'normalized', ...
         'Position', [.05, .4, .4, .2], 'String', 'Done', 'Callback', @(x, y)(assignin('caller', 'done', true)) );
     hdls.done = uicontrol(hdls.fig, 'Style', 'pushbutton', 'Units', 'normalized', ...
-        'Position', [.5, .4, .4, .2], 'String', 'Show Bodies', 'Callback', @(x, y)(assignin('caller', 'bodiestoggle', true)) );
+        'Position', [.5, .4, .4, .2], 'String', 'Toggle Tracks', 'Callback', @(x, y)(assignin('caller', 'toggle_tracks', true)) );
     hdls.reset = uicontrol(hdls.fig, 'Style', 'pushbutton', 'Units', 'normalized', ...
         'Position', [.05, .15, .4, .2], 'String', 'Reset', 'Callback', @(x, y)(assignin('caller', 'reset', true)));
     hdls.loop = uicontrol(hdls.fig, 'Style', 'pushbutton', 'Units', 'normalized', ...
@@ -36,32 +37,34 @@ function plot_tracks( tracks_cell, tracks_names, daterefs_cell, ...
     leg_hdls = zeros(size(daterefs_cell));
     while ~done
         set(0, 'CurrentFigure', ffig)
-        notracks = false;
-        for j = 1:length(daterefs_cell)
-            dateref = daterefs_cell{j};
-            tracks = tracks_cell{j};
-            if isempty(dateref{i})
-                notracks = true;
-                continue;
-            end
-            tidx = dateref{i}(1);
-            cpos = tracks{tidx}(:,3) == i;
-            leg_hdls(j) = plotm(tracks{tidx}(:,1:2), COLORS{j});
-            plotm(tracks{tidx}(cpos,1:2), [COLORS{j} 's']);
-            for k = 2:length(dateref{i})
-                tidx = dateref{i}(k);
+        if show_tracks
+            notracks = false;
+            for j = 1:length(daterefs_cell)
+                dateref = daterefs_cell{j};
+                tracks = tracks_cell{j};
+                if isempty(dateref{i})
+                    notracks = true;
+                    continue;
+                end
+                tidx = dateref{i}(1);
                 cpos = tracks{tidx}(:,3) == i;
-                plotm(repmat([0 (j-1)*0.1], size(tracks{tidx}, 1), 1)+tracks{tidx}(:,1:2), COLORS{j});
-                plotm([0 (j-1)*0.1]+tracks{tidx}(cpos,1:2), [COLORS{j} 's']);
+                leg_hdls(j) = plotm(tracks{tidx}(:,1:2), COLORS{j});
+                plotm(tracks{tidx}(cpos,1:2), [COLORS{j} 's']);
+                for k = 2:length(dateref{i})
+                    tidx = dateref{i}(k);
+                    cpos = tracks{tidx}(:,3) == i;
+                    plotm(repmat([0 (j-1)*0.1], size(tracks{tidx}, 1), 1)+tracks{tidx}(:,1:2), COLORS{j});
+                    plotm([0 (j-1)*0.1]+tracks{tidx}(cpos,1:2), [COLORS{j} 's']);
+                end
+            end
+            if ~notracks
+                legend(leg_hdls, tracks_names);
             end
         end
         slice = ssh(:,:,i);
         slice(contours(:,:,i)) = max(slice(:))+0.1*range(slice(:));
         pcolorm(lat, lon, slice);
-        if ~notracks
-            legend(leg_hdls, tracks_names);
-        end
-        while ~done && ~prevPressed && ~nextPressed && ~loop && ~reset
+        while ~done && ~prevPressed && ~nextPressed && ~loop && ~reset && ~toggle_tracks
             pause(0.1);
         end
         if prevPressed
@@ -82,8 +85,9 @@ function plot_tracks( tracks_cell, tracks_names, daterefs_cell, ...
                 i = i + 1;
                 pause(0.1);
             end
-        elseif bodiestoggle
-            boddiestoggle = false;
+        elseif toggle_tracks
+            toggle_tracks = false;
+            show_tracks = ~show_tracks;
         elseif reset
             i = 1;
             reset = false;
